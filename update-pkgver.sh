9e7b30b10aa3da03ce22e00f21a53921c4d7790c
Simon Brulhart <simon@brulhart.me>
2015-09-17 15:29:37 +0200

    add the script used to update pkgver
    Followed the procedure described in this post:
    https://bbs.archlinux.org/viewtopic.php?pid=1533916#p1533916

---
+++ update-pkgver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+cd "$(dirname "$0")"
+git pull
+echo Last Revisions:
+git log --format=oneline | head -n3
+echo
+echo -n Next version number: 
+read version
+sed -i "s/pkgver=.*/pkgver=${version}/"  PKGBUILD
+mksrcinfo
+git commit PKGBUILD .SRCINFO -m "update to ${version}"
