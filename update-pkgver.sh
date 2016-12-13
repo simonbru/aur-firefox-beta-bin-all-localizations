7ab7584ac3ddb40c7012476d033f974491509208
Simon Brulhart <simon@brulhart.me>
2016-12-13 09:40:20 +0100

    Auto-select last version from feed

--- update-pkgver.sh
+++ update-pkgver.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -e
 
+last_version() {
+    curl -s https://brulhart.me/rss-bridge/\?action\=display\&bridge\=MozillaFirefoxReleasesBridge\&format\=JsonFormat \
+    | jq -r '.[0].title'
+}
+
 cd "$(dirname "$0")/.."
 git pull
 echo Last Revisions:
 git log --format=oneline | head -n3
 echo
-echo -n Next version number: 
-read version
+echo 'Next version number:'
+read -e -i "$(last_version)" version
 sed -i "s/pkgver=.*/pkgver=${version}/"  PKGBUILD
 sed -i "s/pkgrel=.*/pkgrel=1/"  PKGBUILD
 mksrcinfo
