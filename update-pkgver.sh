797d3d286541ba93fca26c98d37e909b8a20ab49
Simon Brulhart <simon@brulhart.me>
2019-01-07 23:17:25 +0100

    WIP

--- update-pkgver.sh
+++ update-pkgver.sh
@@ -5,6 +5,19 @@ last_version() {
     | jq -r '.[0].title'
 }
 
+retrieve_hashes() {
+    version="$1"
+    if [[ "$version" =~ .*rc.* ]]; then
+        base_version="${version%rc*}"
+        rc_num="${version#*rc}"
+        hashes_path="pub/firefox/candidates/${base_version}-candidates/build${rc_num}/SHA256SUMS"
+    else
+        hashes_path="pub/firefox/releases/${version}/SHA256SUMS"
+    fi
+    full_url="https://releases.mozilla.org/${hashes_path}"
+    curl -- "$full_url" | egrep 'linux-.*/.*/firefox-.*\.tar\.bz2'
+}
+
 cd "$(dirname "$0")/.."
 git pull
 echo Last Revisions:
